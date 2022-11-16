10cffebeee91922adaabd4d3f8d9617e4678fd9d
FernandezLJuan <juanlopezfernandes@gmail.com>
2022-11-16 16:22:01 +0100

    FUNCIONAAAAAAAAAAAAAAAAA

--- arbol.py
+++ arbol.py
@@ -40,6 +40,10 @@ class Estanteria():
 	def actualizar_pos(self,pNuevo):
 		
 		self.p0=pNuevo
+	
+	def destino(self):
+
+		return self.p0==self.pf
 
 class Nodo():
 	
@@ -55,18 +59,13 @@ class Nodo():
 	def addNodo(self,mapa,pos,g):
 		
 		novo_nodo=Nodo(self,mapa,pos)
-  
-		if mapa_objetivos[novo_nodo.pos[0]][novo_nodo.pos[1]]==2 and novo_nodo.pos[3]:
-
-			novo_nodo.mapa[novo_nodo.pos[0]][novo_nodo.pos[1]]=1
 
 		distancia=[]
-		for p in pos_entrega:
-			distancia.append(abs((p[0]-novo_nodo.pos[0]))+abs((p[1]-novo_nodo.pos[1])))
+		for p in estanterias:
+			distancia.append(abs((p.pf[0]-novo_nodo.pos[0]))+abs((p.pf[1]-novo_nodo.pos[1])))
 
 		novo_nodo.g=novo_nodo.pai.g+g
 		novo_nodo.f=novo_nodo.g+min(distancia)
-		print(novo_nodo.f)
 
 		if abierta == []:
 
@@ -105,25 +104,20 @@ class Nodo():
 
 			actual=self.comprobar(abierta.pop(0))
 
-
-			if actual.pos==[pos_entrega[0][0],pos_entrega[0][1],0,True]: #or actual.pos==[pos_entrega[1][0],pos_entrega[1][1],0,True] or actual.pos==[pos_entrega[2][0],pos_entrega[2][1],0,True]:
-				actual.mapa[actual.pos[0]][actual.pos[1]]=4
-
-			if actual.mapa[pos_entrega[0][0]][pos_entrega[0][1]]==4: #and actual.mapa[pos_entrega[1][0]][pos_entrega[1][1]]==4 and actual.mapa[3][9]==4:
-				for a in mapa_objetivos:
-					print(a)
+			if estanterias[0].destino():
+				
 				exito=True
 
 			else:
 
-				self.elevador(actual)
+				self.elevador_subir(actual)
 				self.mover(actual)
 				self.girar_izquierda(actual)
 				self.girar_derecha(actual)
+
 				cerrada.append(actual)
-		
+
 		print("Terminamos pibe")
-		print(actual.g)
 		plan=[]
 
 		while actual.pai!=None:
@@ -135,17 +129,22 @@ class Nodo():
   
 		print(plan)
   
-	def elevador(self,actual):
+	def elevador_subir(self,actual):
      
 		pos_temp=actual.pos.copy()
 		nuevo_mapa=actual.mapa.copy()
-     
-		if mapa_objetivos[pos_temp[0]][pos_temp[1]]==2:
 
+		if pos_temp[0]==estanterias[0].p0[0] and pos_temp[1]==estanterias[0].p0[1] and not estanterias[0].destino():
 			pos_temp[3]=True
+
 			actual.addNodo(nuevo_mapa,pos_temp,1)
-      
-		elif mapa_objetivos[pos_temp[0]][pos_temp[1]]==4 or pos_temp[3]:
+
+	def elevador_bajar(self,actual):
+
+		pos_temp=actual.pos.copy()
+		nuevo_mapa=actual.mapa.copy()
+
+		if pos_temp[3]:
 
 			pos_temp[3]=False
 			actual.addNodo(nuevo_mapa,pos_temp,1)
@@ -176,16 +175,9 @@ class Nodo():
 
 			if actual.pos[3]:
 
-				if mapa_objetivos[int(pos_temp[0]+sin(pos_temp[2]*pi/2))][int(pos_temp[1]-cos(pos_temp[2]*pi/2))]==0 or mapa_objetivos[int(pos_temp[0]+sin(pos_temp[2]*pi/2))][int(pos_temp[1]+cos(pos_temp[2]*pi/2))]==0:
-        
-					cabe=False
-        
-				nuevo_mapa[pos_temp[0]][pos_temp[1]]=2
-				nuevo_mapa[actual.pos[0]][actual.pos[1]]=1
+				estanterias[0].actualizar_pos([pos_temp[0],pos_temp[1],0])
     
-			if cabe:
-    
-					actual.addNodo(nuevo_mapa,pos_temp,1)
+			actual.addNodo(nuevo_mapa,pos_temp,1)
 
 	def girar_izquierda(self,actual):
 
@@ -206,8 +198,6 @@ class Nodo():
 		elif pos_temp[2]==1:
 
 			pos_temp[2]=0
-
-		#print("Posicion actual: {}".format(pos_temp))
 
 		actual.addNodo(mapa_objetivos,pos_temp,3)
 
@@ -231,11 +221,7 @@ class Nodo():
 
 			pos_temp[2]=0
 
-		#print("Posicion actual: {}".format(pos_temp))
-
 		actual.addNodo(mapa_objetivos,pos_temp,3)
-
-
 
 root=Nodo(None,mapa_objetivos,[8,4,0,False])
 
